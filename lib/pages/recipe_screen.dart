75db0c706112ce70dceba964ff096cf79a4b6d14
adelnasyrov <anasyrov2017@litsey2.ru>
2022-09-16 11:11:01 +0100

    recipe_screen 11 (info 4d card added)

--- lib/pages/recipe_screen.dart
+++ lib/pages/recipe_screen.dart
@@ -258,6 +258,45 @@ class _RecipeScreenState extends State<RecipeScreen>
                                             textAlign: TextAlign.center)),
                                   ]),
                                 ),
+                              ),
+                              Container(
+                                width: 230,
+                                height: 90,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(12))),
+                                  color: Colors.grey[800],
+                                  child: Column(children: [
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.only(top: 8, bottom: 8),
+                                        child: Text("Жиры",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: "Comfort",
+                                                fontSize: 12),
+                                            textAlign: TextAlign.center)),
+                                    Image.asset(
+                                      'assets/images/fats.png',
+                                      height: 24,
+                                      width: 24,
+                                      color: Colors.white54,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.only(top: 8, bottom: 8),
+                                        child: Text(
+                                            recipe.fats.toString() +
+                                                " ккал/100г",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: "Comfort",
+                                                fontSize: 12),
+                                            textAlign: TextAlign.center)),
+                                  ]),
+                                ),
                               )
                             ],
                           ),
