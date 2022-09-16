0ceb84e0b29cc215f6217ff9d2d188ed7bf8837c
adelnasyrov <anasyrov2017@litsey2.ru>
2022-09-16 11:37:06 +0100

    recipe_screen 11 (info 7th card added)

--- lib/pages/recipe_screen.dart
+++ lib/pages/recipe_screen.dart
@@ -100,7 +100,7 @@ class _RecipeScreenState extends State<RecipeScreen>
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(left: 20, top: 10, right: 20),
+            padding: EdgeInsets.only(left: 10, top: 10, right: 10),
             child: Container(
               width: double.maxFinite,
               height: 480,
@@ -244,6 +244,43 @@ class _RecipeScreenState extends State<RecipeScreen>
                                             EdgeInsets.only(top: 8, bottom: 8),
                                         child: Text(
                                             recipe.fats.toString() + " г/100г",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: "Comfort",
+                                                fontSize: 12),
+                                            textAlign: TextAlign.center)),
+                                  ]),
+                                ),
+                              ),
+                              Container(
+                                width: 230,
+                                height: 90,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(12))),
+                                  color: Colors.grey[850],
+                                  child: Column(children: [
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.only(top: 8, bottom: 8),
+                                        child: Text("Источник",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: "Comfort",
+                                                fontSize: 12),
+                                            textAlign: TextAlign.center)),
+                                    Image.asset(
+                                      'assets/images/link.png',
+                                      height: 24,
+                                      width: 24,
+                                      color: Colors.white54,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.only(top: 8, bottom: 8),
+                                        child: Text(recipe.source.toString(),
                                             style: TextStyle(
                                                 color: Colors.white,
                                                 fontFamily: "Comfort",
